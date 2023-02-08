67a9b459dbaf31e2f637ae3e8d9724cda594f11c
JawaraGordon <jawaragordon@icloud.com>
2023-02-07 19:10:10 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// function to add hidden class to HTML elements
+const initHiddenElements = () => {
+    // variable to select HTML elements - manually add fade-in-on-scroll class to divs
+    const showElements = document.querySelectorAll(".fade-in-on-scroll");
+    console.log("showEls", showElements);
