b0e610424ea9d19d6e75cbd6bc6ebb6882833165
JawaraGordon <jawaragordon@icloud.com>
2023-02-21 19:57:23 -0500

    removed console.logs

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const initHiddenElements = () => {
   // variable to select HTML elements - manually add fade-in-on-scroll class to divs
   const showElements = document.querySelectorAll('.fade-in-on-scroll');
-  console.log('showEls', showElements);
 
   // global variable to set root options (passed as second arg to IntersectionObserver constructor) - "MDN: 50% visibility = 0.5. Can be array [0, 0.25, 0.5, 0.75, 1. 1.0 = threshold isn't considered passed until every pixel is visible."
   let options = {
@@ -20,7 +19,7 @@ const initHiddenElements = () => {
   }, options);
 
   const hiddenElements = document.querySelectorAll('.fade-in-on-scroll');
-  console.log('hiddenEls', hiddenElements);
+
   hiddenElements.forEach((element) => {
     element.classList.add('hidden');
   });
